5c2c59c770f74d6c284b4bf5fc1289de2ae8ed11
Eder <ederv1067@gmail.com>
2022-05-03 23:51:01 -0600

    Mock de la clase paginator

--- tst/main.cpp
+++ tst/main.cpp
@@ -3,7 +3,16 @@
 //
 
 #include "main.h"
-
+TEST(PaginatorTest, PaginatorRequest){
+class MockPaginator : public paginator{
+public:
+    MOCK_METHOD1(get_card, string(int card_num))
+};
+//No se pudo importar la libreria :(
+MockClient mockObject;
+EXPECT_CALL(mockObject, connect("Names"))
+.Times(1);
+}
 
 int main(){
 
